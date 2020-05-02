cf194501be2843b7023d1815eb2d2e5da555919f
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-05-02 07:15:59 -0400

    nest g service tasks --no-spec, connected service to controller, added get all method with empty task array

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service'
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+
+	// dependency injection for the service
+	constructor(private taskService: TasksService) { }
+
+
+	@Get()
+	getAllTasks() {
+		return this.taskService.getAllTasks();
+	}
+}
