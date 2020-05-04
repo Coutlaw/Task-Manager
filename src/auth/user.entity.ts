f4af9a3fdae2092481cf3422248c5d78c3e52156
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-05-04 06:15:43 -0400

    validation on sign in

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -17,7 +17,7 @@ export class User extends BaseEntity {
 	salt: string;
 
 	async validatePassword(password: string): Promise<boolean> {
-		const hash = await bcrypt.has(password, this.salt);
+		const hash = await bcrypt.hash(password, this.salt);
 		return hash ===  this.password;
 	}
 }
